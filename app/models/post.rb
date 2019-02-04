d21d8d3ef83104eaf74b6c0549d7c456eefd2e6e
canikwe <c.anikwe@gmail.com>
2019-02-04 13:38:50 -0500

    updated validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,25 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validate :clickbait
+
+  def clickbait
+    # byebug
+    n = [/Won't Believe/, /Secret/, /Top [0-9]/, /Guess/]
+    if self.title != nil
+      if n.find {|nn| self.title.match(nn)} == nil
+          errors.add(:title, "Not enough clickbait")
+      end
+    end
+    # n = ["Won't Believe", "Secret", "Top [0-9]", "Guess"]
+    # if self.title != nil
+    #   if n.size == (n - self.title.split).size
+    #       errors.add(:title, "Not enough clickbait")
+    #   end
+    # else false
+    # end
+  end
+
 end
